54053df802fe8add77cd11aa7d6ad6a734a40118
xius666 <xiu@ualberta.ca>
2024-03-20 03:37:15 -0400

    add rocket animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,13 +6,16 @@ import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import Hero from "../components/Hero";
 import Highlights from "../components/Highlights";
+import AnimatedObject from "../components/animation/Animation";
 
 export default function MainPage() {
   return (
     <Box>
       <Hero />
-
       <Box sx={{ bgcolor: "background.default" }}>
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <AnimatedObject />
+        </Box>
         <Divider />
         <Highlights />
       </Box>
