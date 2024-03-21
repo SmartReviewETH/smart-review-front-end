314a2c7d20d40fc3ea5f90b44120b44ec21df917
xius666 <xiu@ualberta.ca>
2024-03-20 22:16:43 -0400

    remove console.log

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,6 @@
 import { NFTStorage } from "nft.storage";
 
 export async function Ipfsuploader(file, description) {
-  console.log(process.env.REACT_APP_NFT_STORAGE_TOKEN);
   const client = new NFTStorage({
     token: process.env.REACT_APP_NFT_STORAGE_TOKEN,
   });
